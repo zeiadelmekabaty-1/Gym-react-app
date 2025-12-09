4ebe7d093bd8fb2bfeee2bf1e33119a64487ff75
zeiadelmekabaty-1 <zeyadahmed863@gmail.com>
2025-12-09 08:37:00 +0200

    Add files via upload

---
+++ src/components/Workout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SectionWrapper from "./sectionWrapper";
+import { EXERCISES } from "../utils/swoldier";
+import ExerciseCard from "./ExerciseCard";
+
+export default function Workout(props) {
+  const { workout } = props;
+  return (
+    <SectionWrapper
+      id={'workout'}
+      header={"Welcome to"}
+      title={["The", "DANGER", "zone"]}
+    >
+      <div className="flex flex-col gap-4">
+          {workout.map((exercise, i) => {
+            return (
+              <ExerciseCard i={i} exercise={exercise} key={i} />
+            )
+          })}
+      </div>
+    </SectionWrapper>
+  );
+}
